c6578d9c2a0d66a68f4ea97e949f0d9daa52aa45
Aamir Maniar <aamironline@gmail.com>
2022-12-08 00:37:38 +0530

    AST Parsing WIP

---
+++ ast/utils.go
@@ -0,0 +1,14 @@
+package ast
+
+func ToNodesSlice(v interface{}) []Node {
+	if v == nil {
+		return nil
+	}
+	islice := v.([]interface{})
+	// convert iSlice to []Node
+	nodes := make([]Node, len(islice))
+	for i, node := range islice {
+		nodes[i] = node.(Node)
+	}
+	return nodes
+}
