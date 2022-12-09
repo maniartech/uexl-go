21dc6c9cd58c0f27d6eb6ecdc9e65cec847ed775
Aamir Maniar <aamironline@gmail.com>
2022-12-09 11:43:33 +0530

    Clean up

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"strings"
 
+	"github.com/maniartech/uexl_go/ast"
 	"github.com/maniartech/uexl_go/parser"
 )
 
 // for testing purpose
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: calculator 'EXPR'")
+	}
 
-	_, err := parser.ParseReader("", strings.NewReader("10 + 4"))
+	got, err := parser.ParseReader("", strings.NewReader(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// if len(os.Args) != 2 {
-	// 	log.Fatal("Usage: calculator 'EXPR'")
-	// }
-	// got, err := parser.ParseReader("", strings.NewReader(os.Args[1]))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("=", got)
+	fmt.Println("Evaluation pending, printing the AST")
+	ast.PrintNode(got.(ast.Node))
 }
