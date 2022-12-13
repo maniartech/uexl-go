ed9657a7c1bb0bef6437bde850b81cce2d51f65a
Aamir Maniar <aamironline@gmail.com>
2022-12-13 12:56:35 +0530

    expressions parsing moved to parser package

---
+++ parser/expressions_parser_test.go
@@ -0,0 +1,38 @@
+package parser
+
+import "github.com/maniartech/uexl_go/ast"
+
+func parseExpression(token string, first, rest interface{}, offset, line, col int) (ast.Node, error) {
+
+	l, ok := first.(ast.Node)
+
+	// TODO: handle error
+	if l != nil && !ok { // when l is nil, ignore it!
+		panic("invalid-expression - assertion-failed")
+	}
+
+	restSl := toIfaceSlice(rest)
+
+	for _, v := range restSl {
+		restExpr := toIfaceSlice(v)
+		r, ok := restExpr[3].(ast.Node)
+		// TODO: handle error
+		if !ok {
+			panic("invalid-expression - assertion-failed!")
+		}
+		op := ""
+
+		if o, ok := restExpr[1].([]byte); ok {
+			op = string(o)
+		} else if o, ok := restExpr[1].(string); ok {
+			op = o
+		}
+
+		if op != "" {
+			opType := ast.GetOperatorType(op)
+			l = ast.NewExpressionNode(token, op, opType, l, r, offset, line, col)
+		}
+	}
+
+	return l, nil
+}
