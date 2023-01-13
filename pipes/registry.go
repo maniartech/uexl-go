c59c058169b35c0f91733345bc91fdee14205b5e
Aamir Maniar <aamironline@gmail.com>
2023-01-13 12:57:16 +0530

    Pipe evaluation

---
+++ pipes/registry.go
@@ -0,0 +1,24 @@
+package pipes
+
+import (
+	"github.com/maniartech/uexl_go/core"
+	"github.com/maniartech/uexl_go/types"
+)
+
+type PipeHandler func(evaluator core.Evaluator, context types.Map, prevResult any) (interface{}, error)
+type handlers map[string]PipeHandler
+
+var _handlers = handlers{}
+
+func (r handlers) Register(name string, handler PipeHandler) {
+	r[name] = handler
+}
+
+func (r handlers) Unregister(name string) {
+	delete(r, name)
+}
+
+func Get(name string) (PipeHandler, bool) {
+	handler, ok := _handlers[name]
+	return handler, ok
+}
