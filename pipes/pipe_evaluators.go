c11f1bc54cc533622ebac8aa4444e136db5e99c8
Aamir Maniar <aamironline@gmail.com>
2023-01-14 22:50:28 +0530

    Pipe evaluators, WIP

--- pipes/pipe_evaluators.go
+++ pipes/pipe_evaluators.go
@@ -16,6 +16,14 @@ func firstEvaluator(evaluator core.Evaluator, context types.Map, prevResult any)
 	return evaluator.Eval(context)
 }
 
+// passEvaluator evalues the passes the result of the previous node to the current node.
+func passEvaluator(evaluator core.Evaluator, context types.Map, prevResult any) (interface{}, error) {
+	defer delete(context, "$1")
+
+	context["$1"] = prevResult
+	return evaluator.Eval(context)
+}
+
 // mapEvaluator evalues the map node in the pipe.
 func mapEvaluator(evaluator core.Evaluator, context types.Map, prevResult any) (interface{}, error) {
 	defer delete(context, "$1")
@@ -62,8 +70,35 @@ func filterEvaluator(evaluator core.Evaluator, context types.Map, prevResult any
 	return newArray, nil
 }
 
+// findEvaluator evalues the find node in the pipe.
+func findEvaluator(evaluator core.Evaluator, context types.Map, prevResult any) (interface{}, error) {
+	defer delete(context, "$1")
+
+	array, ok := prevResult.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("filter expects an array")
+	}
+
+	for i := 0; i < len(array); i++ {
+		context["$1"] = array[i]
+		result, err := evaluator.Eval(context)
+		if err != nil {
+			return nil, err
+		}
+		if result.(bool) {
+			return array[i], nil
+		}
+	}
+
+	return nil, nil
+}
+
 func init() {
 	_handlers.Register("first", firstEvaluator)
+	_handlers.Register("pass", passEvaluator)
+
 	_handlers.Register("map", mapEvaluator)
 	_handlers.Register("filter", filterEvaluator)
+	_handlers.Register("find", findEvaluator)
+
 }
