29865511f036dee65c2e9d021edd1ddfa24cd8c9
Aamir Maniar <aamironline@gmail.com>
2023-05-22 21:26:10 +0530

    Added + operator support  in null

--- types/nulls.go
+++ types/nulls.go
@@ -15,14 +15,19 @@ func (n Null) IsTruthy() bool {
 	return false
 }
 
+// IsZero returns true.
+func (n Null) IsZero() bool {
+	return true
+}
+
 // Equals returns true if the null values are equal.
-func (n Null) Equals(other interface{}) bool {
+func (n Null) Equals(other Value) bool {
 	_, ok := other.(Null)
 	return ok
 }
 
 // Compare returns 0 if the null values are equal.
-func (n Null) Compare(other interface{}) (int, error) {
+func (n Null) Compare(other Value) (int, error) {
 	if _, ok := other.(Null); ok {
 		return 0, nil
 	}
@@ -33,4 +38,12 @@ func (n Null) Compare(other interface{}) (int, error) {
 // String returns the string representation of the null value.
 func (n Null) String() string {
 	return "null"
+}
+
+func (n Null) Add(other Value) (Value, error) {
+	if otherV, ok := other.(Array); ok {
+		return append(Array{n}, otherV...), nil
+	}
+
+	return nil, errors.New("invalid type for addition")
 }
