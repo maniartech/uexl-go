167e6bfe53cfb1165a027f7fe64508905e51a5a4
Aamir Maniar <aamironline@gmail.com>
2023-12-30 12:55:42 +0530

    Fix type assertion in object_parser.go

--- parser/object_parser.go
+++ parser/object_parser.go
@@ -14,7 +14,7 @@ func parseObject(vals interface{}, text []byte, offset, line, col int) (ast.Node
 
 	for _, v := range restSl {
 		vSl := toIfaceSlice(v)
-		res = append(res, ast.ObjectItem{Key: vSl[2].(ast.StringNode).Value, Value: vSl[6].(ast.Node)})
+		res = append(res, ast.ObjectItem{Key: vSl[2].(*ast.StringNode).Value, Value: vSl[6].(ast.Node)})
 	}
 
 	return ast.NewObjectNode(string(text), res, offset, line, col), nil
