51e8b25496d733f544b8d2e573e7dc89bf77bdf8
Aamir Maniar <aamironline@gmail.com>
2023-12-31 19:35:11 +0530

    Add toNodesSlice function to convert interface{} to a slice of ast.Node

--- parser/utils.go
+++ parser/utils.go
@@ -12,6 +12,11 @@ func toIfaceSlice(v interface{}) []interface{} {
 	return v.([]interface{})
 }
 
+// toNodesSlice converts an interface{} to a slice of ast.Node.
+// If the input is nil, it returns nil.
+// Otherwise, it assumes that the input is a slice of interfaces and iterates over it,
+// converting each element to an ast.Node and storing it in a new slice.
+// The resulting slice of ast.Node is then returned.
 func toNodesSlice(v interface{}) []ast.Node {
 	if v == nil {
 		return nil
