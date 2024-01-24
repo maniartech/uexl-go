5365b0177dd0ef398228e4b7f4eb7e15f7f4b07f
Aamir Maniar <aamironline@gmail.com>
2024-01-24 22:12:31 +0530

    Fix expression parsing and add test cases

--- parser/expressions_parser_test.go
+++ parser/expressions_parser_test.go
@@ -10,27 +10,38 @@ import (
 
 func TestExpressionParsing(t *testing.T) {
 
-	node, err := parser.ParseString("SUM({ 'x': ['a', 'b', 'c', {'d': x}]}.x.3.d.y1.2, 2)  == 3 + 2")
+	node, err := parser.ParseString("SUM({ 'x': ['a', 'b', 'c', {'d': x}]}.x.3.d.y1.2, 2)  == 3 + 2 ")
+
+	// node, err := parser.ParseString("{ 'a': 1, 'b': 2, 'c': 3, 'd': 4 }")
 
 	// node, err := parser.ParseString("10.53 + ['testing']")
 	if err != nil {
 		t.Errorf("Error: %v", err)
 		return
 	}
-	// fmt.Println(node)
 
-	result, err := node.Eval(types.Context{
-		"x": types.Object{
-			"y": types.Object{
-				"z": types.Number(5),
+	// result, err := node.Eval(types.Context{
+	// 	"x": types.Object{
+	// 		"y": types.Object{
+	// 			"z": types.Number(5),
+	// 		},
+	// 		"y1": types.Array{
+	// 			types.Number(1),
+	// 			types.Number(2),
+	// 			types.Number(3),
+	// 		},
+	// 	},
+	// })
+
+	ctx, _ := types.JSONToContext(`
+		{
+			"y": {
+				"z": 5
 			},
-			"y1": types.Array{
-				types.Number(1),
-				types.Number(2),
-				types.Number(3),
-			},
-		},
-	})
+			"y1": [1, 2, 3],
+		}`)
+
+	result, err := node.Eval(ctx)
 
 	fmt.Println("result", result, err)
 
