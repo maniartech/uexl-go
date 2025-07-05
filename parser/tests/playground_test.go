8893598584d518ded806c3c88b948f135b3189a4
Aamir Maniar <aamironline@gmail.com>
2025-07-05 16:15:42 +0530

    refactor(tests): replace trial_test.go with playground_test.go for improved test structure

---
+++ parser/tests/playground_test.go
@@ -0,0 +1,20 @@
+package parser_test
+
+import (
+	"testing"
+
+	_ "github.com/maniartech/uexl_go/parser"
+)
+
+func TestPlayground(t *testing.T) {
+	// input := "obj.a.b.c + func('10')"
+
+	// parser := NewParser(input)
+	// expr, err := parser.Parse()
+
+	// if err != nil {
+	// 	t.Fatalf("Failed to parse expression: %v", err)
+	// }
+
+	// utils.PrintJSON(expr)
+}
