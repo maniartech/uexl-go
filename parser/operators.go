0fd0bd90b75b13b0c2383d4bdf96cbf9fb1cf227
Aamir Maniar <aamironline@gmail.com>
2025-07-05 13:12:55 +0530

    refactor(parser): remove StartOperator function and associated file

--- parser/operators.go
+++
@@ -1,8 +0,0 @@
-package parser
-
-import "github.com/maniartech/uexl_go/parser/constants"
-
-// startOperator returns true if the given character is the start of an operator.
-func StartOperator(ch rune) bool {
-	return constants.StartOperator(ch)
-}
