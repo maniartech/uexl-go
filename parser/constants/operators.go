d66269df4c6cce9fe500213d25de14fcf1e08b38
Aamir Maniar <aamironline@gmail.com>
2025-07-05 13:14:09 +0530

    refactor(operators): add TODO for unused StartOperator function

--- parser/constants/operators.go
+++ parser/constants/operators.go
@@ -133,6 +133,7 @@ func (o Operator) String() string {
 }
 
 // StartOperator returns true if the given character is the start of an operator
+// TODO: This function is currently not used in the codebase. Check and remove if not needed.
 func StartOperator(ch rune) bool {
 	return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' ||
 		ch == '&' || ch == '|' || ch == '^' || ch == '~' ||
