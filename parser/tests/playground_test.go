c31de1c805648a2f52b1d7598614fc4d90b48803
Aamir Maniar <aamironline@gmail.com>
2025-08-16 12:50:02 +0530

    test(parser): simplify TestPlayground by removing redundant test cases

--- parser/tests/playground_test.go
+++ parser/tests/playground_test.go
@@ -8,25 +8,12 @@ import (
 )
 
 func TestPlayground(t *testing.T) {
-	// Test cases for multiple consecutive operators
-	testCases := []string{
-		"--10",     // double negation
-		"!!true",   // double NOT
-		"!!!false", // triple NOT
-		"!true",    // single NOT
-		"!=",       // should fail (incomplete)
-		"!!x",      // double NOT with variable
+	input := "!!true"
+
+	expr, err := parser.ParseString(input)
+	if err != nil {
+		t.Fatalf("Failed to parse expression: %v", err)
 	}
 
-	for i, input := range testCases {
-		t.Logf("Testing case %d: %s", i+1, input)
-		expr, err := parser.ParseString(input)
-		if err != nil {
-			t.Logf("Case %d failed: %v", i+1, err)
-		} else {
-			t.Logf("Case %d succeeded", i+1)
-			utils.PrintJSON(expr)
-		}
-		t.Log("---")
-	}
+	utils.PrintJSON(expr)
 }
