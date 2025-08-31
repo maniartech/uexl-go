acd79a6862b21b15a67c944213b16cd475bb27bc
Aamir Maniar <aamironline@gmail.com>
2025-08-31 22:17:55 +0530

    test(tokenizer): add unit tests for slicing operator functionality

---
+++ parser/tests/tokenizer_slicing_test.go
@@ -0,0 +1,122 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/maniartech/uexl_go/parser"
+	"github.com/maniartech/uexl_go/parser/constants"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTokenizer_Slicing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []parser.Token
+	}{
+		{
+			input: "arr[1:4]",
+			expected: []parser.Token{
+				{Type: constants.TokenIdentifier, Value: "arr", Token: "arr"},
+				{Type: constants.TokenLeftBracket, Value: "[", Token: "["},
+				{Type: constants.TokenNumber, Value: float64(1), Token: "1"},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenNumber, Value: float64(4), Token: "4"},
+				{Type: constants.TokenRightBracket, Value: "]", Token: "]"},
+				{Type: constants.TokenEOF},
+			},
+		},
+		{
+			input: "arr[:3]",
+			expected: []parser.Token{
+				{Type: constants.TokenIdentifier, Value: "arr", Token: "arr"},
+				{Type: constants.TokenLeftBracket, Value: "[", Token: "["},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenNumber, Value: float64(3), Token: "3"},
+				{Type: constants.TokenRightBracket, Value: "]", Token: "]"},
+				{Type: constants.TokenEOF},
+			},
+		},
+		{
+			input: "arr[1:]",
+			expected: []parser.Token{
+				{Type: constants.TokenIdentifier, Value: "arr", Token: "arr"},
+				{Type: constants.TokenLeftBracket, Value: "[", Token: "["},
+				{Type: constants.TokenNumber, Value: float64(1), Token: "1"},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenRightBracket, Value: "]", Token: "]"},
+				{Type: constants.TokenEOF},
+			},
+		},
+		{
+			input: "arr[:]",
+			expected: []parser.Token{
+				{Type: constants.TokenIdentifier, Value: "arr", Token: "arr"},
+				{Type: constants.TokenLeftBracket, Value: "[", Token: "["},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenRightBracket, Value: "]", Token: "]"},
+				{Type: constants.TokenEOF},
+			},
+		},
+		{
+			input: "arr[0:5:2]",
+			expected: []parser.Token{
+				{Type: constants.TokenIdentifier, Value: "arr", Token: "arr"},
+				{Type: constants.TokenLeftBracket, Value: "[", Token: "["},
+				{Type: constants.TokenNumber, Value: float64(0), Token: "0"},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenNumber, Value: float64(5), Token: "5"},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenNumber, Value: float64(2), Token: "2"},
+				{Type: constants.TokenRightBracket, Value: "]", Token: "]"},
+				{Type: constants.TokenEOF},
+			},
+		},
+		{
+			input: "arr[::-1]",
+			expected: []parser.Token{
+				{Type: constants.TokenIdentifier, Value: "arr", Token: "arr"},
+				{Type: constants.TokenLeftBracket, Value: "[", Token: "["},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenOperator, Value: "-", Token: "-"},
+				{Type: constants.TokenNumber, Value: float64(1), Token: "1"},
+				{Type: constants.TokenRightBracket, Value: "]", Token: "]"},
+				{Type: constants.TokenEOF},
+			},
+		},
+		{
+			input: "arr[-3:-1]",
+			expected: []parser.Token{
+				{Type: constants.TokenIdentifier, Value: "arr", Token: "arr"},
+				{Type: constants.TokenLeftBracket, Value: "[", Token: "["},
+				{Type: constants.TokenOperator, Value: "-", Token: "-"},
+				{Type: constants.TokenNumber, Value: float64(3), Token: "3"},
+				{Type: constants.TokenColon, Value: ":", Token: ":"},
+				{Type: constants.TokenOperator, Value: "-", Token: "-"},
+				{Type: constants.TokenNumber, Value: float64(1), Token: "1"},
+				{Type: constants.TokenRightBracket, Value: "]", Token: "]"},
+				{Type: constants.TokenEOF},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			tokenizer := parser.NewTokenizer(tt.input)
+			tokens := tokenizer.PreloadTokens()
+
+			assert.Equal(t, len(tt.expected), len(tokens), "Number of tokens does not match")
+
+			for i, expectedToken := range tt.expected {
+				if i >= len(tokens) {
+					t.Errorf("Missing token at index %d. Expected %v", i, expectedToken)
+					continue
+				}
+				actualToken := tokens[i]
+				assert.Equal(t, expectedToken.Type, actualToken.Type, "Token type mismatch at index %d", i)
+				assert.Equal(t, expectedToken.Value, actualToken.Value, "Token value mismatch at index %d", i)
+				assert.Equal(t, expectedToken.Token, actualToken.Token, "Token literal mismatch at index %d", i)
+			}
+		})
+	}
+}
